1b5d1be379ae77589fe28786436f25bfd0f6acb0
catalogo12 <96148687+catalogo12@users.noreply.github.com>
2025-04-23 16:19:52 -0600

    Update index.js
    cambios

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req,res) => {
-   res.send('hellow world hoy 15 oct 2024')
+   res.send('hellow world hoy 23 abril 25')
 });
 app.listen(port, () => {
   console.log(`example app listening on port ${port}`)
